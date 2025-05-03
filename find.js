2dbe6ecce1a21f3d6f966b9b86fed3dc6fb69032
swathi <swathivangapandu21@gmail.com>
2025-05-03 16:11:54 +0530

    FIND FUNCTION

---
+++ find.js
@@ -0,0 +1,3 @@
+const name = [1, 2, 3, 4, 55, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const sum = name.find(num => num > 10); // Finds the first number greater than 10
+console.log(sum); // Output: 55
